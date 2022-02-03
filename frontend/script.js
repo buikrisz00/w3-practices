e2be49a8e9791f4e3da7ffbf2d963e668e434e98
Krisztian Bui <buikrisz00@gmail.com>
2022-02-03 16:02:06 +0100

    Javascript basics

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+// function addTwoIntegers (firstInteger, secondInteger) {
+//     //console.log(firstInteger + secondInteger)
+//     //return firstInteger + secondInteger;
+//     if (typeof firstInteger === "string"  && typeof secondInteger === "number") {
+//         return firstInteger + secondInteger;
+//     }
+//     else {
+//         return "Valamelyik argumentum nem szám! Ezért a művelet nem elvégezhető!"
+//     }
+// }
+
+// console.log( addTwoIntegers(12,13) );
+// console.log( addTwoIntegers(1920,85) );
